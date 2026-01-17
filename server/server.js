f2aae837c4bd700939003035460e56ce0ace7ab9
devesh22s <devesh262004@gmail.com>
2026-01-17 17:13:38 +0530

    ready to use

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ app.use(limiter);
 
 // Global Middleware
 app.use(cors({
-    origin: 'http://localhost:5173', // Aapka Frontend URL
+    origin: ['http://localhost:5173', 'https://velora-e-commerce.vercel.app/'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
